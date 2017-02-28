e1809cfac5e9a17e6082b480dcd642666e29670b
DESKTOP-KEINBTN\Kristian <kdyhrb12@student.aau.dk>
2017-02-28 12:17:33 +0100

    meyer spil - kan løfte bægeret nu, problemer med visning på telefon though

---
+++ MeyerSpil/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float m_Speed = 20f;
+
+    private Vector3 moveTo;
+    private Vector3 up = new Vector3(0, 4.5f, 0);
+    private Vector3 down = new Vector3(0, 3, 0);
+
+    public void ToggleRotationDirection()
+    {
+        Debug.Log("Moving cup");
+
+        if (moveTo == up)
+        {
+            moveTo = down;
+        }
+        else
+        {
+            moveTo = up;
+        }
+
+        transform.position = moveTo;
+    }
+
+    void Update()
+    {
+        
+    }
+}
